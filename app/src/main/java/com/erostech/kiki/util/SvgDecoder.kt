0e3b9b1574a7b8043642791712165cc485f317c5
eros902002@googlemail.com <digimon902002>
2017-06-19 18:34:02 +0200

    removed broken SVG decoder class

--- app/src/main/java/com/erostech/kiki/util/SvgDecoder.kt
+++
@@ -1,9 +0,0 @@
-package com.erostech.kiki.util
-
-import com.bumptech.glide.load.ResourceDecoder
-import java.io.InputStream
-
-/**
- * Created by erosgarciaponte on 19.06.17.
- */
-class SvgDecoder : ResourceDecoder<InputStream, Svg
