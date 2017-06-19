2596fe2819984e8bd81bb33e2c42a358027bd01f
Eros Garcia Ponte <eros902002@googlemail.com>
2017-06-19 10:18:01 +0200

    removed credentials

--- app/src/main/java/com/erostech/kiki/Constants.kt
+++ app/src/main/java/com/erostech/kiki/Constants.kt
@@ -3,7 +3,7 @@ package com.erostech.kiki
 /**
  * Created by erosgarciaponte on 16.06.17.
  */
-const val API_TOKEN = "7c26af3aa5f6c0a4ab9f4414787215f3bdd004f80b1b358e72c3137c94f5033c"
-const val PLACEMENT_ID = "pubnative_only"
+const val API_TOKEN = ""
+const val PLACEMENT_ID = ""
 
 const val BASE_API_URL = "https://restcountries.eu/rest/v2/"
